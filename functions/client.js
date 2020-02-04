70f019f868713303569675a28ae4681b28774068
MAC <russellchua95@yahoo.com.sg>
2020-02-04 18:36:16 +0800

    Added a client script for API testing

---
+++ functions/client.js
@@ -0,0 +1,32 @@
+const request = require('request')
+
+//========================GET Request==================================
+
+const optionGET = {
+    method: "GET",
+    url: "http://localhost:5001/complement-4254e/us-central1/app/test",
+    headers: {
+        "Authorization": "Basic ",
+        "Content-Type": "multipart/form-data"
+    }
+};
+
+//========================POST Request==================================
+
+var queryText = "hello"
+
+const optionPOST = {
+    method: "POST",
+    url: "http://localhost:5001/complement-4254e/us-central1/app/testpost",
+    headers: {
+        "Authorization": "Basic ",
+        "Content-Type": "multipart/form-data"
+    },
+    formData : {
+        "input" : queryText
+    }
+};
+
+request(optionGET, function (error, response, body){
+    console.log(response.body);
+})
