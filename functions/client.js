19f0129068356866f6361dcd606c7fb31578a8ab
MAC <russellchua95@yahoo.com.sg>
2020-02-10 18:41:33 +0800

    Added getStars, getQuestions, setNewQuestions

--- functions/client.js
+++ functions/client.js
@@ -40,7 +40,63 @@ const optionPOSTretrieve = {
     }
 };
 
+//========================Upload questions==================================
+var queryText = {   
+                    worldID:"World-1",
+                    section: "1-1",
+                    difficulty: "hard",
+                    questions: "What is log(base 2) 16",
+                    options: [3,4,7,1],
+                    answer: 0
+                }
 
-request(optionPOSTretrieve, function (error, response, body){
+const optionInitializeQuestions = {
+    method: "POST",
+    url: "http://localhost:5001/complement-4254e/us-central1/app/addQuestion",
+    headers: {
+        "Authorization": "Basic ",
+        "Content-Type": "application/json;charset=utf-8"
+    },
+    body : JSON.stringify(queryText)
+    
+};
+
+
+//========================Get Stars /player/world==================================
+var queryText = {   
+    worldID:"World-1",
+    playerID: "U1720925C",
+};
+
+const optionGetStars = {
+    method: "GET",
+    url: "http://localhost:5001/complement-4254e/us-central1/app/getStar",
+    headers: {
+    "Authorization": "Basic ",
+    "Content-Type": "application/json;charset=utf-8"
+    },
+    body : JSON.stringify(queryText)
+
+};
+
+
+//========================Get question set per section==================================
+var queryText = {   
+    worldID:"World-1",
+    sectionID: "1-1",
+};
+
+const optionGetQuestionSet = {
+    method: "GET",
+    url: "http://localhost:5001/complement-4254e/us-central1/app/getques",
+    headers: {
+    "Authorization": "Basic ",
+    "Content-Type": "application/json;charset=utf-8"
+    },
+    body : JSON.stringify(queryText)
+
+};
+
+request(optionGetQuestionSet, function (error, response, body){
     console.log(response.body);
 })
