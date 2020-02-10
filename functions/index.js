095a415b5e208d981e54c67f652821690ef35063
xlepotato <yucia181@gmail.com>
2020-02-10 21:59:18 +0800

    Added getWorldPopulation

--- functions/index.js
+++ functions/index.js
@@ -67,13 +67,19 @@ app.get("/getWorldPopulation", (req,res)=>{
 
     let request = req.body
     let worldID = request.worldID
+    let totalplayer = 0
     
     let databaseRef = database.ref("Maps")
-    // databaseRef = databaseRef.child(worldID)
-    // databaseRef = databaseRef.child(sectionID)
+    databaseRef = databaseRef.child(worldID)
     databaseRef.once("value", function(snapshot){
         let maps = snapshot.val();
-        res.json(maps);
+        Object.keys(maps).forEach(section=>{
+            if (typeof maps[section] !== 'undefined'){
+                totalplayer = totalplayer + Object.keys(maps[section]).length;
+            }
+        })
+        let payload = {worldPopulation: totalplayer}
+        res.json(payload);
     })
 
 });
