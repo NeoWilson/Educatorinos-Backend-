cc485c78d76fb21d23b77da6296829691f8be110
elric <tohwenxiang94@gmail.com>
2020-04-15 16:58:44 +0800

    Sync API calls

--- functions/client/client_elric.js
+++ functions/client/client_elric.js
@@ -33,23 +33,23 @@ const request = require("request");
 //   }
 // };
 
-// /* POST request to update student stars at specified stage */
-// var queryText = {
-//   matric: "U1720925C",
-//   sectionID: "1-1",
-//   stars: "1"
-// };
+/* POST request to update student stars at specified stage */
+var queryText = {
+  matric: "U1720925C",
+  sectionID: "1-1",
+  stars: "2",
+};
 
-// const optionPostUpdateSectionStars = {
-//   method: "POST",
-//   url:
-//     "http://localhost:5001/complement-4254e/us-central1/app/elric/setSectionStars",
-//   headers: {
-//     Authorization: "Basic ",
-//     "Content-Type": "application/json;charset=utf-8"
-//   },
-//   body: JSON.stringify(queryText)
-// };
+const optionPostUpdateSectionStars = {
+  method: "POST",
+  url:
+    "http://localhost:5001/complement-4254e/us-central1/app/elric/setSectionStars",
+  headers: {
+    Authorization: "Basic ",
+    "Content-Type": "application/json;charset=utf-8",
+  },
+  body: JSON.stringify(queryText),
+};
 
 // /* POST request to create student account */
 // var queryText = {
@@ -87,28 +87,26 @@ const request = require("request");
 //   body: JSON.stringify(queryText)
 // };
 
-const optionCheckValidStudent = {
-  method: "GET",
-  url:
-    "http://localhost:5001/complement-4254e/us-central1/app/elric/checkValidStudent/?matric=U1720925C",
-  // "https://us-central1-complement-4254e.cloudfunctions.net/app/elric/checkValidStudent/?matric=U1720925C",
-  headers: {
-    Authorization: "Basic ",
-    "Content-Type": "application/json;charset=utf-8",
-  },
-};
+// const optionCheckValidStudent = {
+//   method: "GET",
+//   url:
+//   // "https://us-central1-complement-4254e.cloudfunctions.net/app/elric/checkValidStudent/?matric=U1720925C",
+//   headers: {
+//     Authorization: "Basic ",
+//     "Content-Type": "application/json;charset=utf-8",
+//   },
+// };
 
-const optionCheckValidTeacher = {
-  method: "GET",
-  url:
-    "http://localhost:5001/complement-4254e/us-central1/app/elric/checkValidTeacher/?teacher_id=E1507788C",
-  // "https://us-central1-complement-4254e.cloudfunctions.net/app/elric/checkValidTeacher/?teacher_id=E1507788C",
-  headers: {
-    Authorization: "Basic ",
-    "Content-Type": "application/json;charset=utf-8",
-  },
-};
+// const optionCheckValidTeacher = {
+//   method: "GET",
+//   url:
+//   // "https://us-central1-complement-4254e.cloudfunctions.net/app/elric/checkValidTeacher/?teacher_id=E1507788C",
+//   headers: {
+//     Authorization: "Basic ",
+//     "Content-Type": "application/json;charset=utf-8",
+//   },
+// };
 
-request(optionCheckValidTeacher, function (error, response, body) {
+request(optionPostUpdateSectionStars, function (error, response, body) {
   console.log(response.body);
 });
