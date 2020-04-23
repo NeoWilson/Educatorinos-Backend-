78b4cd3cbddaaa0f25cfda80c8f8c752e4bb5069
Neo <wilson@10.27.29.106>
2020-04-23 22:53:58 +0800

    Removed unused codes

--- functions/routes/apiWilson.js
+++ functions/routes/apiWilson.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+//========== Get all Worlds' IDs ==============
 
 router.get("/getWorldIds", (req,res)=>{
+	
 	let database = req.app.get('database');
-
 	let databaseRef = database.ref("Maps");
 
 	databaseRef.once("value", function(snapshot) {
@@ -16,33 +17,22 @@ router.get("/getWorldIds", (req,res)=>{
     });
 });
 
+//========== Get Leaderboards for all Worlds ==============
+
 router.get("/getGlobalLeaderboard", (req,res)=>{
+	
 	let database = req.app.get('database');
-<<<<<<< HEAD
-	let databaseRef = database.ref("Players");
-=======
 	let databaseRef = database.ref("Students");
 
->>>>>>> c43185293d175c7f580a1ee7dbed1fec22974111
 	databaseRef.once("value", function(snapshot) {
 		var dict = {};
 		snapshot.forEach(function(Snapshot) {
 			dict[Snapshot.key] = Snapshot.val();
-<<<<<<< HEAD
-		})
-		res.end(JSON.stringify(dict));
-    });
-=======
 			let filename = dict[Snapshot.key].avatar_url + ".jpg"
 			dict[Snapshot.key].avatar_url = `https://storage.googleapis.com/complement-4254e.appspot.com/${filename}`
 		})
-		
 		res.end(JSON.stringify(dict));
     });
-    // res.setHeader("Content-Type", "application/json");
-    // res.end(JSON.stringify(array));
-    // return;
->>>>>>> c43185293d175c7f580a1ee7dbed1fec22974111
 });
 
 module.exports = router;
